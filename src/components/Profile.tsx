fd2b27c70c7710b5ad1ca743d051d762d2def776
Iago Farias <iagox38@gmail.com>
2021-02-26 15:33:26 -0300

    Quarto dia

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import {useContext} from 'react';
+import {ChallengesContext} from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(){
+  const {level} = useContext(ChallengesContext);
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/61480933?s=460&u=032fdbc1bd3e89b8d4016bf25c18e07098ebc988&v=4" alt="Iago Farias"/>
@@ -8,7 +12,7 @@ export function Profile(){
         <strong>Iago Farias</strong>
         <p>
           <img src="icons/level.svg" alt="Level up"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
